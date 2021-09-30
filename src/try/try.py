2146463ffaeab93151e0f2ce39badb22828e545b
weigerzan <jiawei-z19@mails.tsinghua.edu.cn>
2021-09-30 23:03:21 +0800

    add try.py for running

--- src/try/try.py
+++ src/try/try.py
@@ -5,13 +5,15 @@ import subprocess
 import random
 import time
 import math
+import os
 
-lane_path = "lanes-id.json"
-access_path = r"D:\学习\2021秋\城市认知智能挑战赛\access.txt"
-output_path = r"D:\学习\2021秋\城市认知智能挑战赛\输出结果"
-save_result_path = "save-result.json"
+# 将以下路径修改成本地的路径
+lane_path = "lanes-id.json" # lane-id文件
+access_path = r"D:\学习\2021秋\城市认知智能挑战赛\access.txt" # access.txt路径
+output_path = r"D:\学习\2021秋\城市认知智能挑战赛\输出结果" # output文件夹的路径
 
-
+# TODO: 存储每一次最优的结果；打印每一轮最小的时间；去掉减少车道时的输出；当一个初始状态的后继状态都比他好时，破例将其保留
+# TODO: 测试减少初始状态； 减少车道时，至少保留原有的2/3
 # cmd
 my_command = "docker run --mount type=bind,source=" + output_path + ",target=/output --mount type=bind,source=" + access_path + ",target=/access.txt --rm git.tsingroc.com:5050/release/cup2109:latest"
 
@@ -37,17 +39,18 @@ class AutoCmd:
                 if len(start_state[1]) < 50:
                     threshold = 0.9
                     # threshold = 0
-                elif len(start_state[1]) < 100:
+                elif len(start_state[1]) < 250:
                     threshold = 0.7
                 elif len(start_state[1]) < 300:
                     threshold = 0.5
                 else:
                     threshold = 0.3
                 if p > threshold:
-                    my_access = random.sample(start_state[1], random.randint(1, int(len(start_state[1])/2)))
+                    my_access = random.sample(start_state[1], random.randint(int(len(start_state[1])*2/3), len(start_state[1])))
                     print('Too many lanes || Reduce lane')
                 else:
-                    randomroad = random.sample(lanes, random.randint(1, 100))
+                    # my_access = random.sample(start_state[1], random.randint(int(len(start_state[1])/2), len(start_state[1])))
+                    randomroad = random.sample(lanes, random.randint(10, 30))
                     last_access = start_state[1]
                     my_access = list(set(last_access + randomroad))
                 f.close()
@@ -103,7 +106,7 @@ class AutoCmd:
 
 if __name__ == "__main__":
     my_auto_cmd = AutoCmd(waiting_time=420, lanes_length=35189)
-    cycle_times = 3
+    cycle_times = 4
     value = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     with open('start.json', 'r') as f:
         state_list = json.load(f)
@@ -112,17 +115,19 @@ if __name__ == "__main__":
         for statenum in range(len(state_list)):
             state = state_list[statenum]
             print('Previous speed: ' + str(state[0]))
-            print(state[1])
+            # print(state[1])
             for k in range(value[statenum]):
                 starttime = time.time()
                 print("try: " + str(k) + "/" + str(value[statenum]))
                 list_ = my_auto_cmd.generate_access(lane_path ,access_path, state)
+                # break
                 my_auto_cmd.runcmd(my_command, output_path, save_result_path, list_, state[0], i)
                 if (my_auto_cmd.shut_down_flag):
                     print("TimeError: break in " + str(i + 1) + "/" + str(cycle_times))
                     break
                 print('Time: ' + str(time.time() - starttime) + 's')
                 # print("process: " + str(i + 1) + "/" + str(cycle_times))
+            # break
         with open('save-result' + str(i) + '.json', 'r') as f:
             new_state = json.load(f)
             for state in new_state:
@@ -131,6 +136,8 @@ if __name__ == "__main__":
             rlist = sorted(state_list, key=(lambda x: [x[0]]))
             n = min(10, len(rlist))
             state_list = rlist[0:n]
+            state_list = sorted(state_list, key = (lambda x: len(x[1])))
+            state_list = state_list[0:min(10, len(state_list))]
             grade = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             for state in state_list:
                 if len(state) == 3:
@@ -139,5 +146,12 @@ if __name__ == "__main__":
                         if state_list[k][0] == last_state:
                             grade[k] = grade[k] + 1
             for k in range(10):
-                value[k] = 5 + grade[k] * 2
+                value[k] = 3 + grade[k] * 2
+        print("=========================")
+        print("process: " + str(i) + "  best time " + str(state_list[0][0]))
+        print("=========================")
+        bestpath = os.sep.join([os.getcwd(), 'best_result' + str(i) + '.json'])
+        with open(bestpath, 'w') as f:
+            json.dump(state_list, f)
+            f.close()
 # cmd: nohup python3 /home/ubuntu/qry-codespace/city/src/auto-cmd.py &
